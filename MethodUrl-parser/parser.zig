859655a2402b7a87d1dee2bf9c672ce4f3293d2d
Yashbhatt7 <yabh678@gmail.com>
2025-07-15 02:46:25 +0530

    Add

--- MethodUrl-parser/parser.zig
+++ MethodUrl-parser/parser.zig
@@ -1,55 +1,48 @@
 const std = @import("std");
 
-const Expression = struct {
-    number: [10][]const u8,
-    operator: [10][]const u8,
-    num_count: usize,
-    op_count: usize,
+const Request = struct {
+    method: []const u8,
+    url: []const u8,
 };
 
-pub fn parseExpression(input: []const u8) !Expression {
-    var expr = Expression {
-        .number = undefined,
-        .operator = undefined,
-        .num_count = 0,
-        .op_count = 0,
+fn parseRequest(input: []const u8) !Request {
+    const trimmed = std.mem.trim(u8, input, " \t\r\n");
+
+    const space_index = std.mem.indexOfScalar(u8, trimmed, ' ') orelse {
+        return error.InvalidRequest;
     };
 
-    var tokens = [_][]const u8{""} ** 20; // 20 tokens capacity
-    var token_count: usize = 0;
+    const method = trimmed[0..space_index];
 
-    var itr = std.mem.tokenizeScalar(u8, input, ' ');
-    while(itr.next()) |token| {
-        tokens[token_count] = token;
-        token_count += 1;
-    }
+    const url = std.mem.trimLeft(u8, trimmed[space_index + 1 ..], " ");
 
-    for(tokens[0..token_count]) |token| {
-        if(std.mem.eql(u8, token, "+") or std.mem.eql(u8, token, "-")) {
-            expr.operator[expr.op_count] = token;
-            expr.op_count += 1;
-        }
-        else {
-            expr.number[expr.num_count] = token;
-            expr.num_count += 1;
+    const valid_methods = [_][]const u8{ "GET", "POST", "PUT", "DELETE" };
+    var is_valid_method = false;
+    for(valid_methods) |valid_method| {
+        if(std.mem.eql(u8, method, valid_method)) {
+            is_valid_method = true;
+            break;
         }
     }
+    if(!is_valid_method) {
+        return error.InvalidRequest;
+    }
 
-    return expr;
+
+    return Request {
+        .method = method,
+        .url = url,
+    };
 }
 
-pub fn main() void {
-    const input = "12 + 36 - 10";
+pub fn main() !void {
+    const input = "GET http://example.com";
 
-    const result = try parseExpression(input);
+    const result = parseRequest(input) catch |err| {
+        std.debug.print("Error parsing request: {}\n", .{err});
+        return;
+    };
 
-    std.debug.print("Numbers:", .{});
-    for(result.number[0..result.num_count]) |num| {
-        std.debug.print(" {s}", .{num});
-    }
-
-    std.debug.print("\nOperator:", .{});
-    for(result.operator[0..result.op_count]) |ops| {
-        std.debug.print(" {s}", .{ops});
-    }
+    std.debug.print("Method: \"{s}\"\n", .{result.method});
+    std.debug.print("Url: \"{s}\"\n", .{result.url});
 }
